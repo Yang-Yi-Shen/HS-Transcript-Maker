6f370ba7a8f3904a9c471d2f36bc72be47b98360
Yang-Yi-Shen <mrshenyangyi@gmail.com>
2023-04-16 20:48:32 +0800

    feat: created component for homepage

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <main>
+            <h1>High School Transcript Maker</h1>
+            <p>Taking the pain out of making transcripts for homeschoolers</p>
+        </main>
+    )
+}
